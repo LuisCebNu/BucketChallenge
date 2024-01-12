2832d2055241f94be2d264e44f18da5323687a29
Luis Ceballo <66170740+LuisCebNu@users.noreply.github.com>
2024-01-12 17:50:55 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using Microsoft.AspNetCore.Builder;
+
+WebApplicationBuilder Builder =
+    WebApplication.CreateBuilder(args);
+
+WebApplication App =
+    Builder.Build();
+
+App.MapGet("/",
+
+    () => "Sorry, Mario. The Princess is in another castle."
+    );
+
+App.Run();
